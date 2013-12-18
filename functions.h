0a16c959e61915fbc710a3731dd4e7b81d73f129
Sedovlas96 <Egolior@mail.ru>
2013-12-19 02:31:03 +0300

    prototypes

---
+++ functions.h
@@ -0,0 +1,8 @@
+#ifndef FUNCTIONS_H
+#define FUNCTIONS_H
+#include <iostream>
+
+int sizeOfText( char text[] );
+void outputSymbol( int beginIndex, int endIndex, char text[] );
+
+#endif // FUNCTIONS_H
